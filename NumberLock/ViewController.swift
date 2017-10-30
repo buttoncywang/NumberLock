54d011884749e543df8be2b8a65939b4001cd86c
sfc <MIS_SFC@FOXLINK.COM.TW>
2017-10-30 13:23:07 +0800

    1.增加判斷式，用於確認使用者的輸入

--- NumberLock/ViewController.swift
+++ NumberLock/ViewController.swift
@@ -10,20 +10,41 @@ import UIKit
 import GameKit
 
 class ViewController: UIViewController {
-
+    // Make a Random number between 1 to 100
+    var answer=GKRandomSource.sharedRandom().nextInt(upperBound: 100)+1
+    
     @IBOutlet weak var massageLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     
     @IBAction func makeAGuess(_ sender: Any) {
+        print(answer)
+        let inputText=inputTextField.text!
+        let inputNumber=Int(inputText)
         
+        if inputNumber==nil{
+            //Wrong Input
+            print("Wrong Input")
+        }
+        else{
+            //Input Okay
+            if inputNumber!  > 100{
+                print("Too large")
+            }
+            else if inputNumber! < 1{
+                print("Too Small")
+            }
+            else{
+                //Check Answer
+                print("Check Answer")
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //push the keyboard up
         inputTextField.becomeFirstResponder()
-        // Make a Random number between 1 to 100
-        var answer=GKRandomSource.sharedRandom().nextInt(upperBound: 100)+1
+
     }
 
     override func didReceiveMemoryWarning() {
